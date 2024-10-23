a435f53ceb2fcb0a1c6c1b42999e8509787cb73e
Gabriel Vieira <bielvipe@gmail.com>
2024-10-22 22:07:46 -0300

    lowercase path names and references

--- Lib/adder.cpp
+++
@@ -1,9 +0,0 @@
-#include "adder.h"
-
-int israelmath::add(int a, int b) {
-    return a + b;
-}
-
-float israelmath::add(float a, float b) {
-    return a + b;
-}
--- Lib/adder.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace israelmath {
-    int add(int a, int b);
-    float add(float a, float b);
-}
