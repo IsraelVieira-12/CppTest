cc2bccdddd515d803b7cddbab4c157bcb58c4562
Israel <israelvipe@gmail.com>
2024-11-04 17:55:56 -0300

    asser ref

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "Adder/adder.h"
+#include <adder.h>
 #include <GLFW/glfw3.h>
 
 int main(){
--- old/proj/old_main.cpp
+++ old/proj/old_main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "adder.h"
+#include <adder.h>
 
 #include <iostream>
 #include <chrono>
